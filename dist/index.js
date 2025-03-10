ade8feafac95811ebba093c91ff70c8ff65a6492
Hayden <haydenfreeman@yahoo.com>
2025-03-10 15:39:28 -0500

    Auto commit: Mon Mar 10 03:39:28 PM CDT 2025

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.validatePath = validatePath;
+//@ts-ignore
 function validatePath(path) {
     if (path === null) {
         throw new Error("path must be non-null");
