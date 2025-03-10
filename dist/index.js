dfca763affa7c201c2de9493d8bf664413ed8acb
Hayden <haydenfreeman@yahoo.com>
2025-03-10 07:46:23 -0500

    Auto commit: Mon Mar 10 07:46:23 AM CDT 2025

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
 "use strict";
 const fs = require("fs");
+const bencode = require("bencode");
 console.log("hello world");
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 const fs = require("fs");
-const bencode = require("bencode");
 
 console.log("hello world");
