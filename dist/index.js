743df1be6397262c413e2e09fb917d82297249a1
Hayden <haydenfreeman@yahoo.com>
2025-03-10 08:15:17 -0500

    Auto commit: Mon Mar 10 08:15:17 AM CDT 2025

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 "use strict";
 const fs = require("fs");
 const bencode = require("@thi.ng/bencode");
-const torrent = bencode.decode(fs.readFileSync("./puppy.torrent"));
+const torrent = bencode.decode(fs.readFileSync("./puppy.torrent"), false);
 console.log(torrent.isArray());
