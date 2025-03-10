d873492974754b47609438320b6b21cbf08f6295
Hayden <haydenfreeman@yahoo.com>
2025-03-10 08:32:11 -0500

    Auto commit: Mon Mar 10 08:32:11 AM CDT 2025

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const bencode = require("@thi.ng/bencode");
+const dgram = require("dgram");
+const buffer = require("buffer");
+const url = require("url");
+
+const torrent = bencode.decode(fs.readFileSync("./big-buck-bunny.torrent"), false);
+const torrentStr = torrent.announce.toString("utf-8");
+
+const urlStr = url.parse(torrentStr);
+const socket = dgram.createSocket("udp4");
+const msg: Buffer = buffer.Buffer.from("hello", "utf8");
+
+async socket.send(msg, 0, msg.length, urlStr.port, urlStr.host, () => {});
+socket.on("message", (msg: Buffer) => {
+    await console.log(`message is: ${msg}`);
+});
