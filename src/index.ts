5cfb252e6e2a93ddd008a8c612dffc30c480a060
Hayden <haydenfreeman@yahoo.com>
2025-03-10 08:22:07 -0500

    Auto commit: Mon Mar 10 08:22:07 AM CDT 2025

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-const bencode = require("@thi.ng/bencode");
-
-const torrent = bencode.decode(fs.readFileSync("./puppy.torrent"), false);
-console.log(torrent.announce.toString("utf-8"));
