6fff236bd1a2295bc7b9273799b851a393299d9b
Hayden <haydenfreeman@yahoo.com>
2025-03-10 09:45:25 -0500

    Auto commit: Mon Mar 10 09:45:25 AM CDT 2025

--- dist/test.js
+++ dist/test.js
@@ -6,11 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const assert_1 = __importDefault(require("assert"));
 function verifyAllPropsValidTorrent(torrent) {
     for (const key in torrent) {
-        if (!Object.prototype.hasOwnProperty.call(torrent, key)) {
-            continue;
+        if (Object.prototype.hasOwnProperty.call(torrent, key)) {
+            const value = torrent[key];
+            (0, assert_1.default)(value !== null && value !== undefined);
         }
-        const value = torrent[key];
-        (0, assert_1.default)(value !== null && value !== undefined);
     }
 }
 function verifyAllPropsTypesMatchTorrent(torrent) {
