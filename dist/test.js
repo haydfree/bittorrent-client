a1d2b1abf4fc717164eef8bc3be1c876928ee6eb
Hayden <haydenfreeman@yahoo.com>
2025-03-10 15:36:03 -0500

    Auto commit: Mon Mar 10 03:36:03 PM CDT 2025

--- dist/test.js
+++ dist/test.js
@@ -1,22 +1,19 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = __importDefault(require("./index"));
+const index_1 = require("./index");
 function testValidatePath() {
     try {
-        (0, index_1.default)(null);
+        (0, index_1.validatePath)(null);
         console.error("validate path null failed");
     }
     catch (_a) { }
     try {
-        (0, index_1.default)(undefined);
+        (0, index_1.validatePath)(undefined);
         console.error("validate path undefined failed");
     }
     catch (_b) { }
     try {
-        (0, index_1.default)(123);
+        (0, index_1.validatePath)(123);
         console.error("validate path non-string failed");
     }
     catch (_c) { }
