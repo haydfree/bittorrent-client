7d89303be532c94c24d95bcda1d228e7934774b0
Hayden <haydenfreeman@yahoo.com>
2025-03-10 14:53:51 -0500

    Auto commit: Mon Mar 10 02:53:51 PM CDT 2025

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export const torrentFile {
+    path: string,
+    size: number
+}
+
+export const torrentInfo {
+    name: string;
+    size: number;
+    files: torrentFile[];
+    pieces: string;
+    pieceLength: number;
+    private: boolean;
+    creationDate: number;
+    createdBy: string;
+    comment: string;
+    announce: string;
+}
+
