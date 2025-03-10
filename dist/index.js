84b4345dc0e7c65b34d9f16c2a1b16b9dcfde43d
Hayden <haydenfreeman@yahoo.com>
2025-03-10 08:33:23 -0500

    Auto commit: Mon Mar 10 08:33:23 AM CDT 2025

--- dist/index.js
+++ dist/index.js
@@ -6,11 +6,12 @@ const buffer = require("buffer");
 const url = require("url");
 const torrent = bencode.decode(fs.readFileSync("./big-buck-bunny.torrent"), false);
 const torrentStr = torrent.announce.toString("utf-8");
-const urlStr = url.parse(torrentStr);
+/*const urlStr = url.parse(torrentStr);
 const socket = dgram.createSocket("udp4");
-const msg = buffer.Buffer.from("hello", "utf8");
-async;
-socket.send(msg, 0, msg.length, urlStr.port, urlStr.host, () => { });
-socket.on("message", (msg) => {
-    yield console.log(`message is: ${msg}`);
-});
+const msg: Buffer = buffer.Buffer.from("hello", "utf8");
+
+socket.send(msg, 0, msg.length, urlStr.port, urlStr.host, () => {});
+socket.on("message", (msg: Buffer) => {
+    console.log(`message is: ${msg}`);
+});*/
+console.log(torrentStr);
