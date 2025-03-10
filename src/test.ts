933d05a71feaa85f8a2c3408821a57fe8f8259e1
Hayden <haydenfreeman@yahoo.com>
2025-03-10 09:39:50 -0500

    Auto commit: Mon Mar 10 09:39:50 AM CDT 2025

---
+++ src/test.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+import * as bencode from '@thi.ng/bencode';
+import * as dgram from 'dgram';
+import { Buffer } from 'buffer';
+import * as url from 'url';
+import assert from "assert";
+
+import {Torrent, TorrentInfo, TorrentFile} from "./types";
+
+function verifyAllPropsValidTorrent(torrent: Torrent) {
+    for (const key in torrent) {
+        const value = torrent.key;
+        assert(value !== null && value !== undefined);
+    }
+}
+
+function verifyAllPropsTypesMatchTorrent(torrent: Torrent) {
+}
