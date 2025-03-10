6c041d2ba10cfe78b8afb411965ac07594662837
Hayden <haydenfreeman@yahoo.com>
2025-03-10 15:47:45 -0500

    Auto commit: Mon Mar 10 03:47:45 PM CDT 2025

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import * as fs from 'fs';
+import * as bencode from '@thi.ng/bencode';
+import * as dgram from 'dgram';
+import { Buffer } from 'buffer';
+import * as url from 'url';
+
+export function validatePath(path: string) {
+    if (path === null) {throw new Error("path must be non-null")};
+    if (path === undefined) {throw new Error("path must be defined")};
+    if (typeof path !== "string") {throw new Error("path must be string")};
+}
+
+const torrentFilePath = './big-buck-bunny.torrent';
+try {
+    validatePath(torrentFilePath);
+} catch (err) {
+    console.error("path invalid");
+    process.exit(1);
+}
+
+const torrentBytes = fs.readFileSync(torrentFilePath);
+const torrentStr = torrentBytes.announce.toString("utf-8");
+
+console.log(torrentStr);
