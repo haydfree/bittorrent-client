8698aff7cfaf5b69736bc439a9e3ceedeab81ead
Hayden <haydenfreeman@yahoo.com>
2025-03-10 09:34:26 -0500

    Auto commit: Mon Mar 10 09:34:26 AM CDT 2025

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import * as fs from 'fs';
+import * as bencode from '@thi.ng/bencode';
+import * as dgram from 'dgram';
+import { Buffer } from 'buffer';
+import * as url from 'url';
+
+const torrentFilePath = './big-buck-bunny.torrent';
+if (!fs.existsSync(torrentFilePath)) {
+    console.error(`Torrent file not found: ${torrentFilePath}`);
+    process.exit(1);
+}
+console.log(fs.readFileSync(torrentFilePath));
+
