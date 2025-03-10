94cf92f4ef7c4782e302509bf77315181cc4e433
Hayden <haydenfreeman@yahoo.com>
2025-03-10 14:57:36 -0500

    Auto commit: Mon Mar 10 02:57:36 PM CDT 2025

--- dist/types.js
+++ dist/types.js
@@ -1,3 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.boolean = exports. = exports.torrentInfo = exports.number = exports.string = exports.torrentFile = void 0;
+exports.torrentInfo = exports.torrentFile = void 0;
+exports.torrentFile = {
+    path: string,
+    size: number
+};
+exports.torrentInfo = {
+    name: string,
+    size: number,
+    files: exports.torrentFile[],
+    pieces: string,
+    pieceLength: number,
+    private: boolean,
+    creationDate: number,
+    createdBy: string,
+    comment: string,
+    announce: string
+};
--- src/types.ts
+++ src/types.ts
@@ -16,3 +16,5 @@ export const torrentInfo = {
     announce: string
 }
 
+console.log(torrentInfo.name);
+
