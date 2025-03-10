2fbd4b48d7ce848d06995e6e74287ccd5879149b
Hayden <haydenfreeman@yahoo.com>
2025-03-10 15:25:46 -0500

    Auto commit: Mon Mar 10 03:25:46 PM CDT 2025

---
+++ src/test.ts
@@ -0,0 +1,14 @@
+import * as fs from 'fs';
+import * as bencode from '@thi.ng/bencode';
+import * as dgram from 'dgram';
+import { Buffer } from 'buffer';
+import * as url from 'url';
+import * from jest;
+
+it("verifies an error is thrown with null, undefined, or non-string path", () => {
+    expect(validatePath(null)).toThrow("path must be non-null");
+    expect(validatePath(undefined)).toThrow("path must be defined");
+    expect(validatePath(123)).toThrow("path must be string");
+});
+
+
