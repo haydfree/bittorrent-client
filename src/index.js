84c5e9cae3739cfe90cd15ff0c89ec7d6ec9ad03
Hayden <haydenfreeman@yahoo.com>
2025-03-11 13:23:07 -0500

    Auto commit: Tue Mar 11 01:23:07 PM CDT 2025

--- src/index.js
+++ src/index.js
@@ -7,5 +7,3 @@ const torrent = fs.readFileSync(filePath);
 const decodedTorrent = parser.decode(torrent);
 const encodedTorrent = parser.encode(decodedTorrent);
 
-const keys = Object.keys(decodedTorrent);
-keys.forEach(key => console.log(key));
