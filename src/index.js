09441c194cb6488af746ec4219dfb3fef8b05212
Hayden <haydenfreeman@yahoo.com>
2025-03-11 06:38:35 -0500

    Auto commit: Tue Mar 11 06:38:35 AM CDT 2025

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ const tracker = require("./tracker");
 const filePath = "./big-buck-bunny.torrent"
 const buffer = fs.readFileSync(filePath);
 
-// Example usage:
-// const fs = require('fs');
-// const buffer = fs.readFileSync('path_to_torrent_file.torrent');
 const decodedData = bencodeDecode(buffer);
 console.log(decodedData);
 
