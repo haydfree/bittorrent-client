1438713f144b230bf114739880549351b89138bb
Hayden <haydenfreeman@yahoo.com>
2025-03-11 07:39:15 -0500

    Auto commit: Tue Mar 11 07:39:15 AM CDT 2025

--- src/parser.js
+++ src/parser.js
@@ -3,4 +3,12 @@ const bencode = require("@thi.ng/bencode");
 
 module.exports.open = (path) => {
     return bencode.decode(fs.readFileSync(path), false);
-}
+};
+
+module.exports.size = (torrent) => {
+
+};
+
+module.exports.infoHash = (torrent) => {
+
+};
