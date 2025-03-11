e3ca4232f013966870173a2f6b481e9260f1979c
Hayden <haydenfreeman@yahoo.com>
2025-03-11 05:52:51 -0500

    Auto commit: Tue Mar 11 05:52:51 AM CDT 2025

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const bencode = require("@thi.ng/bencode");
+const tracker = require("./tracker");
+
+const filePath = "./big-buck-bunny.torrent"
+const torrent = bencode.decode(fs.readFileSync(filePath), false);
+
+tracker.getPeers(torrent, peers => {
+    console.log(`peers: ${peers}`);
+});
