30a7d831a3f514b335ecc04e56c23fcdbb6f8e6c
Hayden <haydenfreeman@yahoo.com>
2025-03-11 12:46:20 -0500

    Auto commit: Tue Mar 11 12:46:20 PM CDT 2025

---
+++ src/parser.mjs
@@ -0,0 +1,19 @@
+import fs from "fs";
+import crypto from "crypto";
+import bencode from "bencode";
+
+export function decode(torrent) {
+   return bencode.decode(torrent, "utf8");
+};
+
+export function encode(torrent) {
+    return bencode.encode(torrent);
+}
+
+export function size(torrent) {
+    
+};
+
+export function infoHash(torrent) {
+
+};
