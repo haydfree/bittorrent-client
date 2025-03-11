b3905e45ed1ba865eb037d9c42b5f7fd2c85af03
Hayden <haydenfreeman@yahoo.com>
2025-03-11 06:50:35 -0500

    Auto commit: Tue Mar 11 06:50:35 AM CDT 2025

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const bencode = require("@thi.ng/bencode");
+const tracker = require("./tracker");
+const parser = require("./parser");
+
+const filePath = "./big-buck-bunny.torrent"
+const buffer = fs.readFileSync(filePath);
+
+const decodedData = parser.bencodeDecode(buffer);
+
+//announce
+//announce-list
+//comment
+//created by
+//creation date
+//encoding
+//info
+//url-list
+
+console.log(parser.bencodeDecode(decodedData["info"]));
+
+
