46c269ca6ec33a77b4fdcb606b2e651d242abbba
Hayden <haydenfreeman@yahoo.com>
2025-03-11 12:44:49 -0500

    Auto commit: Tue Mar 11 12:44:49 PM CDT 2025

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const tracker = require("./tracker");
+const parser = require("./parser.mjs");
+
+const filePath = "./big-buck-bunny.torrent"
+const decodedTorrent = parser.decode(filePath);
+const encodedTorrent = parser.encode(decodedTorrent);
+
+console.log(decodedTorrent["url-list"]);
