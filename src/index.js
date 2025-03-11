aac0d3b87d855c7c0322d962207328d4726183bf
Hayden <haydenfreeman@yahoo.com>
2025-03-11 12:48:41 -0500

    Auto commit: Tue Mar 11 12:48:41 PM CDT 2025

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ const filePath = "./big-buck-bunny.torrent"
 const torrent = fs.readFileSync(filePath);
 const decodedTorrent = parser.decode(torrent);
 const encodedTorrent = parser.encode(decodedTorrent);
+const da = parser.decode(encodedTorrent);
 
-console.log(parser.decode(encodedTorrent));
+console.log(decodedTorrent);
